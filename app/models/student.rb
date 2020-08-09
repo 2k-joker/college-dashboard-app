2e05e0884b68dfffe3e73c1e60866fc278bb5cb7
Khalil Kum <kum.vanjunior@gmail.com>
2020-08-09 05:17:32 -0400

    Add sevure password to student table

--- app/models/student.rb
+++ app/models/student.rb
@@ -7,4 +7,7 @@ class Student < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: { case_sensitive: false },
     length: { maximum: 105 }, format: { with: VALID_EMAIL_REGEX }
+
+  # Associations
+  has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_09_070247) do
+ActiveRecord::Schema.define(version: 2020_08_09_091035) do
 
   create_table "courses", force: :cascade do |t|
     t.string "course_code"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_08_09_070247) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
