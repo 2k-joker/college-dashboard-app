bf46627860d381a921d0eafe21b63781aac2d662
Khalil Kum <kum.vanjunior@gmail.com>
2020-08-09 02:10:20 -0400

    Modify navbar styling

--- Gemfile
+++ Gemfile
@@ -25,6 +25,9 @@ gem 'jbuilder', '~> 2.5'
 # Use Materialize for frontend
 gem 'materialize-sass', '~> 1.0.0'
 gem 'material_icons'
+
+# Add jquery
+gem 'jquery-rails'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,13 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery
 //= require materialize
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+$( document ).on('turbolinks:load',function(){
+  $(".dropdown-trigger").dropdown();
+  $('.sidenav').sidenav();
+})
