e181ea4ace1259f8047d616c0b246396f4512bd6
Khalil Kum <kum.vanjunior@gmail.com>
2020-08-09 03:49:08 -0400

    create students model, add index view and sign up form

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,9 @@
+class StudentsController < ApplicationController
+  def index
+    @students = Student.all
+  end
+
+  def new
+    @student = Student.new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :students, except: [:destroy]
   root 'courses#index'
   get 'courses/new', to: 'courses#new'
   get 'about', to: 'pages#about'
