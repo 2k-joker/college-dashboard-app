5b85546d79917f27374f9b7c3b46e84eb8489abe
Khalil Kum <kum.vanjunior@gmail.com>
2020-08-09 05:51:53 -0400

    Add user sessions workflow

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,13 @@
+class SessionsController < ApplicationController
+  def create
+
+  end
+
+  def destroy
+
+  end
+
+  def new
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   root 'courses#index'
   get 'courses/new', to: 'courses#new'
   get 'about', to: 'pages#about'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#delete'
 end
