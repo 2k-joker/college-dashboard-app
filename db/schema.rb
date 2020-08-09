9d6bca958ab0002fecd7bc092931d3e4d14a173f
Khalil Kum <kum.vanjunior@gmail.com>
2020-08-09 03:11:22 -0400

    Create student resources and add validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_09_063035) do
+ActiveRecord::Schema.define(version: 2020_08_09_070247) do
 
   create_table "courses", force: :cascade do |t|
     t.string "course_code"
     t.string "name"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
