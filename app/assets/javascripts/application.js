a12c7ce3f87c02fb870710c03883d26dd4aae08a
Khalil Kum <kum.vanjunior@gmail.com>
2020-08-09 04:24:14 -0400

    Fix sign up form layout and add custome messages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,4 +20,5 @@
 $( document ).on('turbolinks:load',function(){
   $(".dropdown-trigger").dropdown();
   $('.sidenav').sidenav();
+  $('#fade-out-target').fadeOut(4000);
 })
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,4 +6,21 @@ class StudentsController < ApplicationController
   def new
     @student = Student.new
   end
+
+  def create
+    @student = Student.new(white_list_params)
+    if @student.save
+      flash[:notice] = 'Successfully signed up'
+      redirect_to root_path
+    else
+      flash[:alert] = 'Whoops! Something went wrong. Please try again'
+      render 'new'
+    end
+  end
+
+  private
+
+  def white_list_params
+    params.require(:student).permit(:name, :email)
+  end
 end
